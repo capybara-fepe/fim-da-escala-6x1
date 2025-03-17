73207c7211c9543b0a15546184e217c89f5ddef7
Fernanda Shinoda <fernanda.shinoda@hotmail.com>
2025-03-17 22:22:07 +0000

    feat(#27): adds main section initial styling

---
+++ src/app/sections/main.tsx
@@ -0,0 +1,15 @@
+export default function Main() {
+	return (
+		<div className="space-y-8 mb-8 h-screen flex flex-col justify-center items-center ">
+			<h1 className="text-7xl font-guildhall w-3xs md:w-full md:max-w-6xl md:text-9xl md:text-center">
+				Fim da escala 6x1
+			</h1>
+			<a
+				className="font-fira-sans py-6 bg-white text-black w-3xs text-center font-bold rounded-full md:text-xl"
+				href="#"
+			>
+				PRESSIONE JÁ
+			</a>
+		</div>
+	)
+}
