bc7cc8213d047268ffa8f07626a6ee4eb28254a6
FShinoda <fernanda.shinoda@hotmail.com>
2025-03-17 14:43:34 -0300

    feat(#26): adds biomejs linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,32 @@
 export default function Page() {
-  return (
-    <div className="p-6">
-    <div className="space-y-1 mb-8">
-      <h1 className="text-7xl font-guildhall">Fim da escala 6x1</h1>
-      <p className="text-xl font-fira-sans w-[800px] text-gray-300">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, iste. Dolores sunt deleniti nemo beatae sequi assumenda, hic in illum commodi minima, cumque temporibus iste rem dolorem aliquam quidem laboriosam.</p>
-    </div>
-    <div className="space-y-1 mb-8">
-      <h1 className="text-7xl font-guildhall">Fim da escala 6x1</h1>
-      <p className="text-xl font-jost  w-[800px] text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sint pariatur, perspiciatis reprehenderit id consequatur ex quidem! In, velit error voluptatibus officiis atque et eos odio non, ex laudantium labore!</p>
-    </div>
-    <div className="space-y-1 mb-8">
-      <h1 className="text-7xl font-guildhall">Fim da escala 6x1</h1>
-      <p className="text-xl w-[800px] text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam veritatis tenetur dolorum quis. Iusto nam molestias a? At, iste quo eos officia sunt dolore, illum dolores natus, asperiores iusto rerum?</p>
-    </div>
-    </div>
-  )
+	return (
+		<div className="p-6">
+			<div className="space-y-1 mb-8">
+				<h1 className="text-7xl font-guildhall">Fim da escala 6x1</h1>
+				<p className="text-xl font-fira-sans w-[800px] text-gray-300">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus,
+					iste. Dolores sunt deleniti nemo beatae sequi assumenda, hic in illum
+					commodi minima, cumque tempor ibus iste rem dolorem aliquam quidem
+					laboriosam.
+				</p>
+			</div>
+			<div className="space-y-1 mb-8">
+				<h1 className="text-7xl font-guildhall">Fim da escala 6x1</h1>
+				<p className="text-xl font-jost  w-[800px] text-gray-300">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+					sint pariatur, perspiciatis reprehenderit id consequatur ex quidem!
+					In, velit error voluptatibus officiis atque et eos odio non, ex
+					laudantium labore!
+				</p>
+			</div>
+			<div className="space-y-1 mb-8">
+				<h1 className="text-7xl font-guildhall">Fim da escala 6x1</h1>
+				<p className="text-xl w-[800px] text-gray-300">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+					veritatis tenetur dolorum quis. Iusto nam molestias a? At, iste quo
+					eos officia sunt dolore, illum dolores natus, asperiores iusto rerum?
+				</p>
+			</div>
+		</div>
+	)
 }
